7c19821436bbd43d10dbc1e56d55711735343579
antokhio <antokhio@gmail.com>
2023-03-28 22:12:21 +0300

    feat: migration strapi 4.8.2

--- dist/server/content-types/category/lifecycles.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    async beforeCreate(event) {
-        event.params.data.order =
-            (await strapi.db.query("plugin::categorizer.category").count(undefined)) *
-                10;
-    },
-};
--- server/controllers/category.ts
+++ server/controllers/category.ts
@@ -2,6 +2,22 @@
  *  controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('plugin::categorizer.category');
+const normalizeMany = (categories: any[]) => categories.map((category) => normalize(category));
+const normalize = ({ id, title, slug, parent }: any) => ({
+    id,
+    title,
+    slug,
+    parent: parent ? parent.id : null,
+});
+
+export default factories.createCoreController('plugin::categorizer.category', ({ strapi }) => ({
+    async findAll(ctx) {
+        const categories = await strapi.db
+            .query('plugin::categorizer.category')
+            .findMany({ populate: ['parent'] });
+
+        return normalizeMany(categories);
+    },
+}));
